476639715b06f57664f64fd4fc3a9f22c2da5a49
Miroslav <miroslav.manovic@gmail.com>
2020-02-08 17:54:47 +0100

    Added form submit

--- burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -110,7 +110,7 @@ class ContactData extends Component {
             .post("/orders.json", order)
             .then(response => {
                 this.setState({ loading: false });
-                this.props.history.push("/");
+                this.props.history.push("/burger-builder");
             })
             .catch(error => {
                 this.setState({ loading: false });
